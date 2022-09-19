81c5f74d9852668768be26028c55d29830a0d760
DebiChowdhury <debi.chowdhury@takeda.com>
2022-09-19 11:26:20 +0530

    Added base component

--- fe1/src/component/Header.js
+++ fe1/src/component/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
     <>
       <TopHeader />
       <Navbar bg="light" expand="lg" className={styles.navbar}>
-        <div class="col">
+        <div className="col">
           <Navbar.Brand href="/home" className={styles.logo}>
             <Image
               src={logo}
@@ -32,7 +32,7 @@ const Header = () => {
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <div class="col">
+          <div className="col">
             <Nav className={"me-auto " + styles.sideNavoptions}>
               {data.main_header_data.navOptions.map((item) => {
                 return (
@@ -66,7 +66,7 @@ const Header = () => {
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-search"
+                className="bi bi-search"
                 viewBox="0 0 16 16"
               >
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
@@ -81,7 +81,7 @@ const Header = () => {
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-bag"
+                className="bi bi-bag"
                 viewBox="0 0 16 16"
               >
                 <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
--- fe1/src/component/TopHeader.js
+++ fe1/src/component/TopHeader.js
@@ -3,13 +3,13 @@ import data from "../../public/data/data.json";
 const TopHeader = () => {
 
   return (
-    <div class="container">
-      <div class="row justify-content-md-center"></div>
-      <div class="row">
-        <div class="col">{data.mini_header_data.country_name} | {data.mini_header_data.language}</div>
-        <div class="col">{data.mini_header_data.banner_data}</div>
-        <div class="col-md-auto">{data.mini_header_data.distributor_text}</div>
-        <div class="col-md-auto">{data.mini_header_data.sign_in}</div>
+    <div className="container">
+      <div className="row justify-content-md-center"></div>
+      <div className="row">
+        <div className="col">{data.mini_header_data.country_name} | {data.mini_header_data.language}</div>
+        <div className="col">{data.mini_header_data.banner_data}</div>
+        <div className="col-md-auto">{data.mini_header_data.distributor_text}</div>
+        <div className="col-md-auto">{data.mini_header_data.sign_in}</div>
       </div>
     </div>
   );
